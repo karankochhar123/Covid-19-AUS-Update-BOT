6732159b9cd7eac6730d93610b32c80ec5eda748
karankochhar123 <52896602+karankochhar123@users.noreply.github.com>
2020-03-30 16:22:33 +1100

    Initial upload
    Config file containing parameters

---
+++ config.py
@@ -0,0 +1,10 @@
+API_key = 'your twitter key'
+API_secret_key = 'your twitter secret key'
+Access_token = 'your twitter access token'
+Access_token_secret = 'your twitter token secret'
+
+URL = 'https://corona.lmao.ninja/countries/'
+
+hashtag = '#COVID19Aus #COVID19'
+
+countries = ['australia','USA','Germany','Spain','Italy','India','UK','Canada','Malaysia','Iran','China','France']
